a83641c545ae6551495a1d95287c08f289e2da84
hadumbae <cmcdnn6919@gmail.com>
2025-04-28 11:11:21 +0700

    - Update `HookFormInput.tsx` to add `hasLabel` boolean flag to hide labels

--- src/common/components/forms/HookFormInput.tsx
+++ src/common/components/forms/HookFormInput.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/common/components/ui/form.tsx";
 import {Input} from "@/common/components/ui/input.tsx";
 import {Control} from "react-hook-form";
+import {cn} from "@/common/lib/utils.ts";
 
 interface Props {
     name: string,
@@ -20,16 +21,33 @@ interface Props {
     type?: string;
     min?: number;
     max?: number;
+    className?: string;
     disabled?: boolean;
+    hasLabel?: boolean;
 }
 
-const HookFormInput: FC<Props> = ({name, label, description, placeholder, control, step, type, min, max, disabled}) => {
+const HookFormInput: FC<Props> = (params) => {
+    const {
+        name,
+        label,
+        description,
+        placeholder,
+        control,
+        step,
+        type,
+        min,
+        max,
+        disabled,
+        className,
+        hasLabel = true,
+    } = params;
+
     return (
         <FormField
             control={control}
             name={name}
-            render={({field}) => <FormItem>
-                <FormLabel>{label}</FormLabel>
+            render={({field}) => <FormItem className={cn(className)}>
+                {hasLabel && <FormLabel>{label}</FormLabel>}
 
                 <FormControl>
                     <Input
@@ -50,8 +68,8 @@ const HookFormInput: FC<Props> = ({name, label, description, placeholder, contro
                     </FormDescription>
                 }
 
-                <FormMessage />
-            </FormItem>} />
+                <FormMessage/>
+            </FormItem>}/>
     );
 };
 
