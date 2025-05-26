ea81c8d93fe4ea3ddc78a1b00f7d12ba1eef8fd7
hadumbae <cmcdnn6919@gmail.com>
2025-05-26 14:45:22 +0700

    refactor(movie): Update Movie Param hook to return null
    Update the `useFetchMovieParams.ts` hook to properly return `null` when `movieID is missing.`

--- src/pages/movies/hooks/params/useFetchMovieParams.ts
+++ src/pages/movies/hooks/params/useFetchMovieParams.ts
@@ -1,16 +1,27 @@
 import {useNavigate, useParams} from "react-router-dom";
 import {toast} from "react-toastify";
+import {IDStringSchema, ObjectId} from "@/common/schema/strings/IDStringSchema.ts";
+import {useEffect} from "react";
 
-export default function useFetchMovieParams() {
+interface ParamReturns {
+    movieID: ObjectId;
+}
+
+export default function useFetchMovieParams(): ParamReturns | null {
     const navigate = useNavigate();
-    const {movieID} = useParams<{movieID: string}>();
+    const {movieID} = useParams<{ movieID: ObjectId }>();
+    const {success, data} = IDStringSchema.safeParse(movieID);
 
-    if (!movieID) {
-        toast.error("Invalid Movie ID.");
-        navigate("/admin/movies");
-    }
+    useEffect(() => {
+        if (!success || !data) {
+            toast.error("Invalid Movie ID.");
+            navigate("/admin/movies");
+        }
+    }, [success, data, navigate]);
+
+    if (!success || !data) return null;
 
     return {
-        movieID,
+        movieID: data!,
     };
 }
