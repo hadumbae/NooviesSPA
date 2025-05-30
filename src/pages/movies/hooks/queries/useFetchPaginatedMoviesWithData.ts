2897cb3fdb1f3b36438869a40a5cb08112d225db
hadumbae <cmcdnn6919@gmail.com>
2025-05-30 21:25:21 +0700

    refactor(movies): Fix import path
    Fix import path for `useFetchPaginatedMovies.ts` and `useFetchPaginatedMoviesWithData.ts`.

--- src/pages/movies/hooks/queries/useFetchPaginatedMoviesWithData.ts
+++ src/pages/movies/hooks/queries/useFetchPaginatedMoviesWithData.ts
@@ -1,4 +1,4 @@
-import {PaginatedMovies, PaginatedMovieSchema} from "@/pages/movies/schema/model/MoviePaginationSchema.ts";
+import {PaginatedMovies, PaginatedMovieSchema} from "@/pages/movies/schema/model/pagination/MoviePaginationSchema.ts";
 import MovieQueryRepository from "@/pages/movies/repositories/MovieQueryRepository.ts";
 import filterEmptyAttributes from "@/common/utility/filterEmptyAttributes.ts";
 import useFetchValidatedDataWithRedirect from "@/common/hooks/validation/useFetchValidatedDataWithRedirect.ts";
