df778bb7542af3000c8b9084b7f4658e86bca824
hadumbae <cmcdnn6919@gmail.com>
2025-06-29 20:02:20 +0700

    refactor(screens): Clean up Theatre Screen Search Params
    Clean up `useTheatreScreenSearchParams`, adding in return type and default values.

--- src/pages/screens/hooks/theatre-screens/params/useTheatreScreenSearchParams.ts
+++ src/pages/screens/hooks/theatre-screens/params/useTheatreScreenSearchParams.ts
@@ -1,13 +1,32 @@
-import {TheatreScreenSearchParamSchema} from "@/pages/screens/schema/params/TheatreScreenParams.schema.ts";
+import {TheatreScreenSearchParamsSchema} from "@/pages/screens/schema/params/TheatreScreenParams.schema.ts";
 import {useSearchParams} from "react-router-dom";
 import fetchParsedSearchParams from "@/common/utility/search-params/fetchParsedSearchParams.ts";
 import setSearchParamValue from "@/common/utility/search-params/setSearchParamValue.ts";
+import {TheatreScreenSearchParams} from "@/pages/screens/schema/params/TheatreScreenParams.types.ts";
 
-export default function useTheatreScreenSearchParams() {
-    const [searchParams, setSearchParams] = useSearchParams();
+type FetchSearchParams = {
+    activeTab?: "seats" | "showings";
+    seatPage?: string;
+    seatsPerPage?: string;
+    showingPage?: string;
+    showingsPerPage?: string;
+}
+
+type ParamReturns = {
+    searchParams: TheatreScreenSearchParams;
+    setSearchParams: () => void;
+    setActiveTab: (value: string | number) => void;
+    setSeatPage: (value: string | number) => void;
+    setSeatsPerPage: (value: string | number) => void;
+    setShowingPage: (value: string | number) => void;
+    setShowingsPerPage: (value: string | number) => void;
+}
+
+export default function useTheatreScreenSearchParams(defaultValues?: FetchSearchParams): ParamReturns {
+    const [searchParams, setSearchParams] = useSearchParams(defaultValues);
 
     const rawData = Object.fromEntries(searchParams.entries());
-    const parsedSearchParams = fetchParsedSearchParams({schema: TheatreScreenSearchParamSchema, raw: rawData});
+    const parsedSearchParams = fetchParsedSearchParams({schema: TheatreScreenSearchParamsSchema, raw: rawData});
 
     const setActiveTab = (value: string | number) => setSearchParamValue({
         key: "activeTab", value, searchParams, setSearchParams
