813f8709a870e520545e431f74382fef3ac99042
hadumbae <cmcdnn6919@gmail.com>
2025-07-08 17:21:39 +0700

    refactor(seat and movie credits): Remove unused schemas and interfaces.
    Remove `MovieCredit` schema utility and `Seat` interfaces.

--- src/pages/seats/interfaces/ISeat.ts
+++
@@ -1,9 +0,0 @@
-import ITheatre from "@/pages/theatres/interfaces/ITheatre.ts";
-import {IScreen} from "@/pages/screens/interfaces/IScreen.ts";
-import {ObjectId} from "@/common/schema/strings/IDStringSchema.ts";
-import ISeatBase from "@/pages/seats/interfaces/ISeatBase.ts";
-
-export default interface ISeat extends ISeatBase {
-    screen: ObjectId | IScreen;
-    theatre: ObjectId | ITheatre;
-}
--- src/pages/seats/interfaces/ISeatDetails.ts
+++
@@ -1,8 +0,0 @@
-import ITheatre from "@/pages/theatres/interfaces/ITheatre.ts";
-import {IScreen} from "@/pages/screens/interfaces/IScreen.ts";
-import ISeatBase from "@/pages/seats/interfaces/ISeatBase.ts";
-
-export default interface ISeatDetails extends ISeatBase {
-    screen: IScreen;
-    theatre: ITheatre;
-}
