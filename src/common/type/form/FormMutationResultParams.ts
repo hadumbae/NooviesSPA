3e2058532f577ea98ffe49ee4fc1c11716b3831b
hadumbae <cmcdnn6919@gmail.com>
2025-07-08 22:18:08 +0700

    refactor(common): Add schema to mutation params
    Add validation schema to parameters for form mutations. This validation is optional, with the parsed results from the backend returned if absent.

--- src/common/type/form/FormMutationResultParams.ts
+++ src/common/type/form/FormMutationResultParams.ts
@@ -1,13 +1,15 @@
 import {ObjectId} from "@/common/schema/strings/IDStringSchema.ts";
+import {ZodTypeAny} from "zod";
 
-export type FormMutationOnSubmitParams<TData = any> = {
+export type FormMutationOnSubmitParams<TData = unknown, TSchema extends ZodTypeAny = ZodTypeAny> = {
+    validationSchema?: TSchema;
     successMessage?: string;
     onSubmitSuccess?: (data: TData) => void;
     errorMessage?: string;
     onSubmitError?: (error: Error) => void;
 }
 
-export type FormMutationResultParams<TData = any> = FormMutationOnSubmitParams<TData>
+export type FormMutationResultParams<TData = unknown, TSchema extends ZodTypeAny = ZodTypeAny> = FormMutationOnSubmitParams<TData, TSchema>
 & (| {
     isEditing: true;
     _id: ObjectId;
