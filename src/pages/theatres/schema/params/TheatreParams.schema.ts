0699a3d4fdd4fad5a9edf50b05a6b0070289926e
hadumbae <cmcdnn6919@gmail.com>
2025-07-13 16:20:27 +0700

    refactor(theatre): Remove old schema
    Remove `TheatreParams.schema.ts`.

--- src/pages/theatres/schema/params/TheatreParams.schema.ts
+++
@@ -1,7 +0,0 @@
-import {z} from "zod";
-import {IDStringSchema} from "@/common/schema/strings/IDStringSchema.ts";
-
-export const TheatreParamSchema = z.object({
-    theatreID: IDStringSchema,
-});
-
