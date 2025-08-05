3b378d352eacd53d4e5f49bf83fa70d535bde311
hadumbae <cmcdnn6919@gmail.com>
2025-08-05 22:41:56 +0700

    fix(common): Fix Header Button
    Fix `HeaderButton` with the correct referencing.

--- src/common/components/page/headers/HeaderButton.tsx
+++ src/common/components/page/headers/HeaderButton.tsx
@@ -1,4 +1,4 @@
-import {FC, PropsWithChildren} from 'react';
+import {forwardRef, PropsWithChildren} from 'react';
 import {cn} from "@/common/lib/utils.ts";
 import ButtonVariant from "@/common/type/ui/ButtonVariant.ts";
 import {Button} from "@/common/components/ui/button.tsx";
@@ -10,20 +10,24 @@ interface Props {
     disabled?: boolean;
 }
 
-const HeaderButton: FC<PropsWithChildren<Props>> = ({children, className, onClick, disabled, variant = "outline"}) => {
-    return (
-        <Button
-            variant={variant}
-            onClick={onClick}
-            disabled={disabled}
-            className={cn(
-                "text-neutral-400 hover:text-black p-2",
-                className
-            )}
-        >
-            {children}
-        </Button>
-    );
-};
+const HeaderButton = forwardRef<HTMLButtonElement, PropsWithChildren<Props>>(
+    ({children, className, onClick, disabled, variant = "outline"}, ref) => {
+        return (
+            <Button
+                ref={ref}
+                variant={variant}
+                onClick={onClick}
+                disabled={disabled}
+                className={cn(
+                    "text-neutral-400 hover:text-black p-2",
+                    className
+                )}
+            >
+                {children}
+            </Button>
+        );
+    })
+
+HeaderButton.displayName = "HeaderButton";
 
 export default HeaderButton;
