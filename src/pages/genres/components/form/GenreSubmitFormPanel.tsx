1b1d986820adc7644873246820a7256c9240d5e1
hadumbae <cmcdnn6919@gmail.com>
2025-08-05 22:45:13 +0700

    feat(genre): Create Genre Form Panel
    Create `GenreSubmitFormPanel` to allow for it the form to be slot into other components.

---
+++ src/pages/genres/components/form/GenreSubmitFormPanel.tsx
@@ -0,0 +1,71 @@
+import {FC, ReactNode, useState} from 'react';
+import {FormMutationOnSubmitParams} from "@/common/type/form/FormMutationResultParams.ts";
+import {Genre} from "@/pages/genres/schema/genre/Genre.types.ts";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger
+} from "@/common/components/ui/sheet.tsx";
+import {GenreFormValues} from "@/pages/genres/schema/form/GenreForm.types.ts";
+import {ScrollArea} from "@/common/components/ui/scroll-area.tsx";
+import GenreSubmitFormContainer from "@/pages/genres/components/form/GenreSubmitFormContainer.tsx";
+
+type EditingProps =
+    | { isEditing: true, genre: Genre }
+    | { isEditing?: false, genre?: never };
+
+type PanelProps = Omit<FormMutationOnSubmitParams, "onSubmitSuccess" | "onSubmitError"> & EditingProps & {
+    children?: ReactNode;
+    onSubmitSuccess?: (genre: Genre) => void;
+    onSubmitError?: (error: unknown) => void;
+    presetValues?: Partial<GenreFormValues>;
+    disableFields?: (keyof GenreFormValues)[];
+    // className?: string;
+};
+
+const GenreSubmitFormPanel: FC<PanelProps> = (params) => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    const {children, onSubmitSuccess, ...formOptions} = params;
+    const {isEditing} = formOptions;
+
+    const sheetTitle = `${isEditing ? "Update" : "Create"} Genre`;
+    const sheetDescription = `${isEditing ? "Update" : "Create"} genres by submitting data.`;
+
+    const defaultOpen = (
+        <span className="text-neutral-400 hover:text-black cursor-pointer">
+            Open
+        </span>
+    );
+
+    const closeOnSubmit = (genre: Genre) => {
+        setOpen(false);
+        onSubmitSuccess && onSubmitSuccess(genre);
+    }
+
+    return (
+        <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger asChild>
+                {children ? children : defaultOpen}
+            </SheetTrigger>
+            <SheetContent className="flex flex-col">
+                <SheetHeader>
+                    <SheetTitle>{sheetTitle}</SheetTitle>
+                    <SheetDescription>{sheetDescription}</SheetDescription>
+                </SheetHeader>
+
+                <ScrollArea className="flex-grow">
+                    <GenreSubmitFormContainer
+                        onSubmitSuccess={closeOnSubmit}
+                        {...formOptions}
+                    />
+                </ScrollArea>
+            </SheetContent>
+        </Sheet>
+    );
+};
+
+export default GenreSubmitFormPanel;
