0fb4a286c792ab6e7662d93e01029034634f238e
hadumbae <cmcdnn6919@gmail.com>
2025-09-09 20:59:36 +0700

    fix(common): Fix dialog title issue in admin sidebar
    Fix issue by removing `SidebarTitle`.

--- src/common/components/sidebar/AdminSidebar.tsx
+++ src/common/components/sidebar/AdminSidebar.tsx
@@ -9,15 +9,10 @@ import AdminTheatreSidebarGroup from "@/common/components/sidebar/admin/AdminThe
 import AdminMovieSidebarGroup from "@/common/components/sidebar/admin/AdminMovieSidebarGroup.tsx";
 import AdminAuthSidebarGroup from "@/common/components/sidebar/auth/AuthSidebarGroup.tsx";
 import AdminSetupSidebarGroup from "@/common/components/sidebar/admin/AdminSetupSidebarGroup.tsx";
-import {SheetTitle} from "@/common/components/ui/sheet.tsx";
 
 const AdminSidebar: FC = () => {
     return (
         <Sidebar>
-            <SheetTitle className="sr-only" >
-                Noovies Sidebar
-            </SheetTitle>
-
             <SidebarHeader>
                 Noovies
             </SidebarHeader>
