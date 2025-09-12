da5148735737a8f3e5a79c366fd1688dca0b9834
hadumbae <cmcdnn6919@gmail.com>
2025-09-12 17:02:58 +0700

    feat(MovieCredit): Remove hooks
    Remove hooks that are no longer used.

--- src/pages/moviecredit/hooks/queries/movies-and-credits/useFetchMoviesAndPersonsParams.ts
+++
@@ -1,8 +0,0 @@
-import {RequestOptions} from "@/common/type/repositories/EntityRequestParamTypes.ts";
-import {PersonQueryFilters} from "@/pages/persons/schema/queries/PersonFilter.types.ts";
-import {MovieQueryFilters} from "@/pages/movies/schema/queries/MovieFilter.types.ts";
-
-export type UseFetchMoviesAndPersonsParams = RequestOptions & {
-    personFilters?: PersonQueryFilters;
-    movieFilters?: MovieQueryFilters;
-}
