e44811fae6a282d4610521da908eefa7c6d22310
hadumbae <cmcdnn6919@gmail.com>
2026-02-18 14:42:17 +0700

    refactor(user): clean up old favourites logic
    Clean up old logic to make way for new ones.

--- src/pages/movies/schema/client/favourites/FavouriteMovieAndShowingSchema.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod";
-import {ShowingSchema} from "@/pages/showings/schema/showing/Showing.schema.ts";
-import {FavouriteMovieSchema} from "@/pages/movies/schema/client/favourites/FavouriteMovieSchema.ts";
-import {IDStringSchema} from "@/common/schema/strings/object-id/IDStringSchema.ts";
-
-export const FavouriteMovieAndShowingSchema = z.object({
-    movie: z.lazy(() => FavouriteMovieSchema),
-    showings: z.array(z.union([IDStringSchema, z.lazy(() => ShowingSchema)])),
-});
