695bcd17f1709d933159f0ef220295b24249bd7c
hadumbae <cmcdnn6919@gmail.com>
2026-03-05 23:37:31 +0700

    feat: create header for movie reviews page

---
+++ src/features/client/movies/pages/movie-info-reviews/sections/MovieInfoReviewsPageHeader.tsx
@@ -0,0 +1,54 @@
+/**
+ * @file Header component for the Movie Reviews page.
+ *
+ * MovieInfoReviewsPageHeader.tsx
+ */
+
+import HeaderTitle from "@/common/components/page/headers/HeaderTitle.tsx";
+import HeaderDescription from "@/common/components/page/headers/HeaderDescription.tsx";
+import PosterImage from "@/pages/movies/components/images/PosterImage.tsx";
+import {URLString} from "@/common/schema/strings/URLStringSchema.ts";
+import LoggedLink from "@/common/components/navigation/logged-link/LoggedLink.tsx";
+
+/**
+ * Props for MovieInfoReviewsPageHeader.
+ */
+type HeaderProps = {
+    /**
+     * Optional poster image URL for the movie.
+     */
+    posterURL?: URLString;
+
+    /**
+     * Slug identifier for the movie.
+     * Used to navigate back to the movie detail page.
+     */
+    movieSlug: string;
+
+    /**
+     * Display title of the movie.
+     */
+    movieTitle: string;
+};
+
+/**
+ * Renders the header section for the Movie Info Reviews page.
+ */
+const MovieInfoReviewsPageHeader = (
+    {movieSlug, movieTitle, posterURL}: HeaderProps
+) => {
+    return (
+        <header className="flex space-x-4 items-end">
+            <LoggedLink to={`/browse/movies/${movieSlug}`}>
+                <PosterImage src={posterURL} className="h-28"/>
+            </LoggedLink>
+
+            <div className="space-y-1 py-3">
+                <HeaderDescription className="text-sm">{movieTitle}</HeaderDescription>
+                <HeaderTitle className="text-2xl">Movie Reviews</HeaderTitle>
+            </div>
+        </header>
+    );
+};
+
+export default MovieInfoReviewsPageHeader;
