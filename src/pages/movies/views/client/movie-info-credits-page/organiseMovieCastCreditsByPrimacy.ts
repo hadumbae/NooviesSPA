ddc44d450afa2b9d2ad448937c5ab7ffb29bbd3b
hadumbae <cmcdnn6919@gmail.com>
2026-03-12 20:23:56 +0700

    feat: create utility that organise cast credits by primacy

---
+++ src/pages/movies/views/client/movie-info-credits-page/organiseMovieCastCreditsByPrimacy.ts
@@ -0,0 +1,62 @@
+/**
+ * @file Groups cast credits into primary and supporting collections.
+ * @filename organiseMovieCastCreditsByPrimacy.ts
+ */
+
+import {
+    CastCreditExceptMovie
+} from "@/pages/moviecredit/schemas/model/credit-except-schemas/CreditExceptMovie.types.ts";
+
+/**
+ * Parameters for organising cast credits.
+ */
+type PrimacyParams = {
+    /** Cast credits to group */
+    credits: CastCreditExceptMovie[];
+}
+
+/**
+ * Result of cast credit grouping.
+ */
+type PrimacyReturns = {
+    /** Primary cast ordered by billing priority */
+    primaryCast: CastCreditExceptMovie[];
+
+    /** Supporting cast ordered alphabetically */
+    supportCast: CastCreditExceptMovie[];
+}
+
+/**
+ * Organises cast credits by primacy and applies ordering rules.
+ */
+export function organiseMovieCastCreditsByPrimacy(
+    {credits}: PrimacyParams
+): PrimacyReturns {
+    const primaryCredits = [];
+    const supportCredits = [];
+
+    for (const credit of credits) {
+        credit.isPrimary
+            ? primaryCredits.push(credit)
+            : supportCredits.push(credit);
+    }
+
+    primaryCredits.sort((a, b) => {
+        if (a.billingOrder === undefined && b.billingOrder === undefined) {
+            return 0;
+        } else if (a.billingOrder === undefined) {
+            return 1;
+        } else if (b.billingOrder === undefined) {
+            return -1;
+        }
+
+        return a.billingOrder - b.billingOrder
+    });
+
+    supportCredits.sort((a, b) => a.person.name.localeCompare(b.person.name));
+
+    return {
+        primaryCast: primaryCredits,
+        supportCast: supportCredits,
+    };
+}
