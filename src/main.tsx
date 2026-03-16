62c24e238b1ac26918150ca0b2e23afbc6f5625c
hadumbae <cmcdnn6919@gmail.com>
2026-03-16 14:56:23 +0700

    feat: add geolocation provider to component tree

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import {QueryClientProvider} from "@tanstack/react-query";
 import AuthProvider from "@/domains/auth/context/AuthProvider.tsx";
 import ReactQueryClient from "@/config/ReactQueryClient.ts";
 import ThemeProvider from "@/common/context/theme/ThemeProvider.tsx";
+import IPGeolocationContextProvider from "@/common/context/ip-geolocation-context/IPGeolocationContextProvider.tsx";
 
 const router = createBrowserRouter(RegisterRoutes);
 
@@ -16,7 +17,9 @@ createRoot(document.getElementById('root')!).render(
         <QueryClientProvider client={ReactQueryClient}>
             <AuthProvider>
                 <ThemeProvider>
-                    <RouterProvider router={router}/>
+                    <IPGeolocationContextProvider>
+                        <RouterProvider router={router}/>
+                    </IPGeolocationContextProvider>
                 </ThemeProvider>
             </AuthProvider>
         </QueryClientProvider>
