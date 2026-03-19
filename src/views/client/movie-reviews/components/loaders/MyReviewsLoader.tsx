6834cbcb50aaf2fd659e89a6f115ddb4c904eddf
hadumbae <cmcdnn6919@gmail.com>
2026-03-19 23:36:52 +0700

    feat: create loader for user's reviews

---
+++ src/views/client/movie-reviews/components/loaders/MyReviewsLoader.tsx
@@ -0,0 +1,45 @@
+/**
+ * @file Data loader component for fetching and validating the current user's movie reviews.
+ * @filename MyReviewsLoader.tsx
+ */
+
+import ValidatedDataLoader from "@/common/components/query/ValidatedDataLoader.tsx";
+import {useFetchMyMovieReviews} from "@/domains/review/fetch/my-reviews/useFetchMyMovieReviews.ts";
+import {PaginatedPopulatedMovieReviewsSchema} from "@/domains/review/schemas/models/MovieReviewRelated.schema.ts";
+import {ReactNode} from "react";
+import {PaginatedPopulatedMovieReviews} from "@/domains/review/schemas/models/MovieReviewRelated.types.ts";
+
+/**
+ * Props for {@link MyReviewsLoader}.
+ */
+type LoaderProps = {
+    /** The current page number for pagination. */
+    page: number;
+    /** The number of reviews to fetch per page. */
+    perPage: number;
+    /** Render-prop function called with the validated {@link PaginatedPopulatedMovieReviews} data.*/
+    children: (data: PaginatedPopulatedMovieReviews) => ReactNode
+}
+
+/**
+ * Orchestrates the data-fetching and validation lifecycle for "My Reviews".
+ * @param props - {@link LoaderProps}
+ */
+const MyReviewsLoader = ({page, perPage, children}: LoaderProps) => {
+    const query = useFetchMyMovieReviews({
+        page,
+        perPage,
+        config: {
+            virtuals: true,
+            populate: true,
+        }
+    });
+
+    return (
+        <ValidatedDataLoader query={query} schema={PaginatedPopulatedMovieReviewsSchema}>
+            {children}
+        </ValidatedDataLoader>
+    );
+};
+
+export default MyReviewsLoader;
