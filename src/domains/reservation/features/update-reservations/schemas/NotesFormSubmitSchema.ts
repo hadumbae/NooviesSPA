ae7f83e54ef5908c6ee3fa34b89e79dc8d3f6592
hadumbae <cmcdnn6919@gmail.com>
2026-03-30 02:35:16 +0700

    refactor: preprocess input before note submit schema

--- src/domains/reservation/features/update-reservations/schemas/NotesFormSubmitSchema.ts
+++ src/domains/reservation/features/update-reservations/schemas/NotesFormSubmitSchema.ts
@@ -5,13 +5,16 @@
 
 import {z} from "zod";
 import {NonEmptyStringSchema} from "@/common/schema/strings/simple-strings/NonEmptyStringSchema.ts";
+import preprocessEmptyStringToUndefined from "@/common/utility/schemas/preprocessEmptyStringToUndefined.ts";
 
 /**
  * Validation schema for the reservation notes update form.
  */
 export const ReservationNotesFormSubmitSchema = z.object({
     /** The administrative text to be associated with the reservation record. */
-    notes: NonEmptyStringSchema.optional().nullable(),
+    notes: preprocessEmptyStringToUndefined(
+        NonEmptyStringSchema.optional().nullable()
+    ),
 });
 
 /**
