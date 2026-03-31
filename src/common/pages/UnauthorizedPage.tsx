3fd82d5eef3f99dfffdc4d535aa368a6cbcb73b6
hadumbae <cmcdnn6919@gmail.com>
2026-03-31 22:11:23 +0700

    style: update `UnauthorizedPage` styling

--- src/common/pages/UnauthorizedPage.tsx
+++ src/common/pages/UnauthorizedPage.tsx
@@ -1,31 +1,28 @@
 import {FC} from 'react';
 import PageCenter from "@/common/components/page/PageCenter.tsx";
-import {Link} from "react-router-dom";
-import {cn} from "@/common/lib/utils.ts";
+import HoverLink from "@/common/components/navigation/HoverLink.tsx";
 
 const UnauthorizedPage: FC = () => {
     return (
         <PageCenter className="space-y-5">
-            <h1 className="font-dotGothic16 text-[100px]">
+            <h1 className="font-dotGothic16 text-[50px] lg:text-[100px]">
                 403
             </h1>
 
-            <h2 className="text-neutral-500">
-                You must be logged in to access this route.
-            </h2>
+            <div className="flex flex-col items-center space-y-2">
+                <h2 className="text-neutral-500">
+                    You must be logged in to access this route.
+                </h2>
 
-            <Link
-                to="/auth/login"
-                className={cn(
-                    "text-neutral-500",
-                    "underline",
-                    "underline-offset-8",
-                    "hover:text-black",
-                    "hover:font-bold",
-                )}
-            >
-                Login
-            </Link>
+                <HoverLink
+                    to="/auth/login"
+                    className="uppercase italic"
+                >
+                    Login
+                </HoverLink>
+            </div>
+
+
         </PageCenter>
     );
 };
