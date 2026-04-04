128d36f7b67b7672e69a8562616245cdad5a2b83
hadumbae <cmcdnn6919@gmail.com>
2026-04-04 16:09:30 +0700

    fix: update base url to proper api url

--- src/domains/customers/features/profile-overview/repositories/repository.ts
+++ src/domains/customers/features/profile-overview/repositories/repository.ts
@@ -2,7 +2,7 @@ import {UserUniqueCode} from "@/domains/users/schemas/UserUniqueCodeSchema.ts";
 import buildQueryURL from "@/common/utility/query/buildQueryURL.ts";
 import useFetchAPI from "@/common/utility/features/use-fetch-api/useFetchAPI.ts";
 
-const baseURL = `${import.meta.env.VITE_BASE_URL}/api/v1/admin/customers/view-data`;
+const baseURL = `${import.meta.env.VITE_API_URL}/api/v1/admin/customers/view-data`;
 
 export const getFetchCustomerProfileViewData = (code: UserUniqueCode) => {
     const url = buildQueryURL({
