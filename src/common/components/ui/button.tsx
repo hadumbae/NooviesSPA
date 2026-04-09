ce346d6396e9894d84855e70f1c067240bee726e
hadumbae <cmcdnn6919@gmail.com>
2026-04-09 22:10:58 +0700

    feat: simplify button css

--- src/common/components/ui/button.tsx
+++ src/common/components/ui/button.tsx
@@ -3,21 +3,17 @@ import {Slot} from "@radix-ui/react-slot"
 import {cva, type VariantProps} from "class-variance-authority"
 
 import {cn} from "@/common/lib/utils"
-import {PrimaryButtonCSS, SecondaryButtonCSS} from "@/common/constants/css/ButtonCSS.ts";
 
 const buttonVariants = cva(
     "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-neutral-300",
     {
         variants: {
             variant: {
-                default:
-                    "bg-neutral-900 text-neutral-50 shadow hover:bg-neutral-900/90 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90",
-                primary: PrimaryButtonCSS,
-                destructive:
-                    "bg-red-500 text-neutral-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90",
-                outline:
-                    "border border-neutral-200 bg-white shadow-sm hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
-                secondary: SecondaryButtonCSS,
+                default: "bg-neutral-900 text-neutral-50 shadow hover:bg-neutral-900/90 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90",
+                primary: "shadcn-primary-button",
+                destructive: "shadcn-destructive-button",
+                outline: "shadcn-outline-button",
+                secondary: "shadcn-secondary-button",
                 ghost: "hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
                 link: "text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50",
                 ghostRing: "ghost-ring-button",
@@ -28,6 +24,7 @@ const buttonVariants = cva(
                 lg: "h-10 rounded-md px-8",
                 icon: "h-9 w-9",
                 fab: "h-12 w-12 rounded-full",
+                tile: "w-full h-32"
             },
         },
         defaultVariants: {
