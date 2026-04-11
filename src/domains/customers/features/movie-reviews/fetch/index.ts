9f001cce956e29b7ffdc41e0963c90ed643e59fc
hadumbae <cmcdnn6919@gmail.com>
2026-04-11 16:43:51 +0700

    feat: create fetch hooks for customer reviews view data

---
+++ src/domains/customers/features/movie-reviews/fetch/index.ts
@@ -0,0 +1,7 @@
+import {
+    useFetchCustomerReviewsViewData
+} from "@/domains/customers/features/movie-reviews/fetch/useFetchCustomerReviewsViewData.ts";
+
+export {
+    useFetchCustomerReviewsViewData
+}
---
+++ src/domains/customers/features/movie-reviews/fetch/useFetchCustomerReviewsViewData.ts
@@ -0,0 +1,51 @@
+/**
+ * @file React Query hook for retrieving paginated movie reviews for a specific customer.
+ * @filename useFetchCustomerReviewsViewData.ts
+ */
+
+import {UseQueryOptions} from "@/common/type/query/UseQueryOptions.ts";
+import {
+    CustomerReviewsViewData,
+    CustomerReviewsViewDataSchema
+} from "@/domains/customers/features/movie-reviews/schemas";
+import {UserUniqueCode} from "@/domains/users/schemas/UserUniqueCodeSchema.ts";
+import {PaginationValues} from "@/common/features/fetch-pagination-search-params";
+import {useQuery, UseQueryResult} from "@tanstack/react-query";
+import HttpResponseError from "@/common/errors/HttpResponseError.ts";
+import {CustomerViewQueryKeys, getFetchCustomerReviewsViewData} from "@/domains/customers/features/data";
+import {buildQueryFn} from "@/common/features/validate-fetch-data";
+import useQueryOptionDefaults from "@/common/utility/query/useQueryOptionDefaults.ts";
+
+/**
+ * Parameters for the useFetchCustomerReviewsViewData hook.
+ */
+type FetchParams = {
+    /** The validated unique code of the customer author. */
+    customerCode: UserUniqueCode;
+    /** Current pagination state (page, perPage) to control the data slice. */
+    pagination: PaginationValues;
+    /** Standard React Query options for caching, stale time, and behavior overrides. */
+    options?: UseQueryOptions<CustomerReviewsViewData>;
+}
+
+/**
+ * Manages the server-state and validation for a customer's paginated review history.
+ * ---
+ * @param params - The configuration object for identity and pagination.
+ * @returns {UseQueryResult} The standard React Query result object containing
+ * data, status, and fetch methods.
+ */
+export function useFetchCustomerReviewsViewData(
+    {customerCode, pagination, options}: FetchParams
+): UseQueryResult<CustomerReviewsViewData, HttpResponseError> {
+    const fetchDetails = buildQueryFn({
+        schema: CustomerReviewsViewDataSchema,
+        action: () => getFetchCustomerReviewsViewData({customerCode, pagination})
+    });
+
+    return useQuery({
+        queryKey: CustomerViewQueryKeys.reviews({customerCode, ...pagination}),
+        queryFn: fetchDetails,
+        ...useQueryOptionDefaults(options),
+    });
+}
