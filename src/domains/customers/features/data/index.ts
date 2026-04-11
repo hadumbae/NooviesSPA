91263c5aa18c91c8809a620301e24e3dddbff7a0
hadumbae <cmcdnn6919@gmail.com>
2026-04-11 16:26:47 +0700

    feat: create repository route for customer reviews view data

--- src/domains/customers/features/data/index.ts
+++ src/domains/customers/features/data/index.ts
@@ -1,10 +1,22 @@
 import {
-    getFetchCustomerProfileViewData, getFetchCustomerReviewViewData
+    getFetchCustomerProfileViewData, getFetchCustomerReviewsViewData, getFetchCustomerReviewViewData
 } from "@/domains/customers/features/data/repository.ts";
 import {CustomerViewQueryKeys} from "@/domains/customers/features/data/queryKeys.ts";
+import {
+    GetFetchCustomerProfileViewDataConfig,
+    GetFetchCustomerReviewsViewDataConfig, GetFetchCustomerReviewViewDataConfig
+} from "@/domains/customers/features/data/repository.types.ts";
 
 export {
     CustomerViewQueryKeys,
     getFetchCustomerProfileViewData,
     getFetchCustomerReviewViewData,
+    getFetchCustomerReviewsViewData,
+}
+
+
+export type {
+    GetFetchCustomerProfileViewDataConfig,
+    GetFetchCustomerReviewsViewDataConfig,
+    GetFetchCustomerReviewViewDataConfig,
 }
