9482b1573db07d124c93a803b74d77acc1e8175d
hadumbae <cmcdnn6919@gmail.com>
2026-04-15 00:40:15 +0700

    feat: create genre navigation hooks

---
+++ src/domains/genres/_feat/navigation/index.ts
@@ -0,0 +1,7 @@
+import {useNavigateToGenreDetails} from "@/domains/genres/_feat/navigation/useNavigateToGenreDetails.ts";
+import {useNavigateToGenreIndex} from "@/domains/genres/_feat/navigation/useNavigateToGenreIndex.ts";
+
+export {
+    useNavigateToGenreIndex,
+    useNavigateToGenreDetails,
+}
