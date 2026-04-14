3f8b1aeb7e2d9b346974b876b20790e553c11bec
hadumbae <cmcdnn6919@gmail.com>
2026-04-14 13:01:46 +0700

    feat: update genre query hook

--- src/domains/genres/_feat/crud-hooks/useFetchGenre.ts
+++ src/domains/genres/_feat/crud-hooks/useFetchGenre.ts
@@ -4,21 +4,23 @@
  * consistent query key management.
  */
 
-import GenreRepository from "@/domains/genres/repositories/GenreRepository.ts";
 import {useQuery, UseQueryResult} from "@tanstack/react-query";
-import useQueryFnHandler from "@/common/utility/query/useQueryFnHandler.ts";
 import HttpResponseError from "@/common/errors/HttpResponseError.ts";
 import {ObjectId} from "@/common/schema/strings/object-id/IDStringSchema.ts";
 import {RequestOptions} from "@/common/type/request/RequestOptions.ts";
 import {UseQueryOptions} from "@/common/type/query/UseQueryOptions.ts";
 import useQueryOptionDefaults from "@/common/utility/query/useQueryOptionDefaults.ts";
 import {GenreCRUDQueryKeys} from "@/domains/genres/_feat/crud-hooks/GenreCRUDQueryKeys.ts";
+import {ZodType, ZodTypeDef} from "zod";
+import {buildQueryFn} from "@/common/features/validate-fetch-data";
+import {findByID} from "@/domains/genres/_feat/crud";
 
 /**
  * Parameters for the useFetchGenre hook.
  */
 export type FetchParams<TData = unknown> = {
     _id: ObjectId;
+    schema: ZodType<TData, ZodTypeDef, unknown>;
     config?: Omit<RequestOptions, "limit">;
     options?: UseQueryOptions<TData>;
 };
@@ -26,12 +28,12 @@ export type FetchParams<TData = unknown> = {
 /**
  * Custom hook for retrieving a single genre via the Genre repository.
  */
-export default function useFetchGenre(
-    {_id, config, options}: FetchParams
-): UseQueryResult<unknown, HttpResponseError> {
-    const fetchGenre = useQueryFnHandler({
-        action: () => GenreRepository.get({_id, config}),
-        errorMessage: "Failed to fetch the genre data. Please try again.",
+export default function useFetchGenre<TData = unknown>(
+    {_id, schema, config, options}: FetchParams<TData>
+): UseQueryResult<TData, HttpResponseError> {
+    const fetchGenre = buildQueryFn({
+        action: () => findByID({_id, config}),
+        schema,
     });
 
     return useQuery({
