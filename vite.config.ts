5726926bde8bea4750704e138b5bda9587ac8200
hadumbae <cmcdnn6919@gmail.com>
2026-04-15 13:02:12 +0700

    feat: install `vitest`

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    server: {
+        port: 3000,
     },
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+    }
 })
