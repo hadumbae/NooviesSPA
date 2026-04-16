19a5354b85df9f5a8b8eba66e249cb0234c4abc7
hadumbae <cmcdnn6919@gmail.com>
2026-04-16 19:40:18 +0700

    fix: fix genre route imports

--- src/domains/genres/routing/ClientGenreRoutes.tsx
+++ src/domains/genres/routing/ClientGenreRoutes.tsx
@@ -2,7 +2,7 @@ import {RouteObject} from "react-router-dom";
 import BaseLayout from "@/common/layout/base-layout/BaseLayout.tsx";
 import ComponentErrorHandler from "@/common/components/errors/ComponentErrorHandler.tsx";
 import BrowseGenreInfoPage from "@/views/client/genres/browse-genre-info/BrowseGenreInfoPage.tsx";
-import { BrowseGenresPage } from "src/views/client/genres/browse-genres-page";
+import { BrowseGenresPage } from "@/views/client/genres/browse-genres-page";
 
 const routes: RouteObject[] = [
     {
