7e85c9656929749c30dc2f973f85f750f36a587b
hadumbae <cmcdnn6919@gmail.com>
2026-04-16 23:45:50 +0700

    refactor: update genre admin view data base url

--- src/domains/genres/_feat/admin-view-data/baseURL.ts
+++ src/domains/genres/_feat/admin-view-data/baseURL.ts
@@ -5,4 +5,4 @@
 /**
  * The base URL used for fetching aggregated administrative data related to genres.
  */
-export const GenreAdminViewDataBaseURL = `${import.meta.env.VITE_API_URL}/api/v1/admin/genres/view-data`;
+export const GenreAdminViewDataBaseURL = `${import.meta.env.VITE_API_URL}/api/v1/views/${import.meta.env.VITE_DEV_CLIENT_NAME}/admin/genres`;
