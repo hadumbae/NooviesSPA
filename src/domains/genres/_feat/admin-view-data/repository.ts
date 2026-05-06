611647849afd69e27b5489eb181152bc7bee8e5b
hadumbae <cmcdnn6919@gmail.com>
2026-05-06 14:44:13 +0700

    refactor: tidy up genre admin view

--- src/domains/genres/_feat/admin-view-data/repository.ts
+++ src/domains/genres/_feat/admin-view-data/repository.ts
@@ -4,10 +4,10 @@
  * aggregate genre metadata with nested collections (e.g., movies).
  */
 
-import buildQueryURL from "@/common/utility/query/buildQueryURL.ts";
 import {FetchGenreDetailsConfig} from "@/domains/genres/_feat/admin-view-data/repository.types.ts";
 import useFetchAPI from "@/common/utility/features/use-fetch-api/useFetchAPI.ts";
 import {GenreAdminViewDataBaseURL} from "@/domains/genres/_feat/admin-view-data/baseURL.ts";
+import {buildURL} from "@/common/features/fetch-api";
 
 /**
  * Fetches aggregated genre details and associated movie data from the administrative API.
@@ -15,9 +15,9 @@ import {GenreAdminViewDataBaseURL} from "@/domains/genres/_feat/admin-view-data/
 export const getFetchGenreDetails = async (
     {slug, queries}: FetchGenreDetailsConfig
 ) => {
-    const url = buildQueryURL({
+    const url = buildURL({
         baseURL: GenreAdminViewDataBaseURL,
-        path: `item/${slug}/details`,
+        path: `/item/${slug}/details`,
         queries,
     });
 
