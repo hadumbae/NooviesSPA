7cd67e4809a1b639b9705b235da2c6586674260f
hadumbae <cmcdnn6919@gmail.com>
2026-05-10 22:43:55 +0700

    feat: create genre image banner component

---
+++ src/views/admin/genres/_comp/images/GenreImageBanner.tsx
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview Component for displaying a genre banner image with fallback states.
+ */
+import {ReactElement, useState} from "react";
+import {CloudinaryImage} from "@/common/schema/models/cloudinary-image/CloudinaryImageSchema.ts";
+import {cn} from "@/common/lib/utils.ts";
+import {ImageOff} from "lucide-react";
+
+/** Props for the GenreImageBanner component. */
+type BannerProps = {
+    image?: CloudinaryImage | null;
+    genreName: string;
+    className?: string;
+};
+
+/**
+ * Renders a genre banner image or a placeholder if the image is missing or fails to load.
+ */
+export function GenreImageBanner(
+    {image, genreName, className}: BannerProps
+): ReactElement {
+    const [hasError, setHasError] = useState<boolean>(false);
+
+    if (!image) {
+        return (
+            <div className={cn("invalid-image-container w-full h-72", className)}>
+                <ImageOff/> {hasError && <span>Failed To Fetch Image</span>}
+            </div>
+        )
+    }
+
+    return (
+        <img
+            src={image.secure_url}
+            alt={`Image for '${genreName}'`}
+            loading="lazy"
+            onError={() => setHasError(true)}
+            className={cn(
+                "w-full h-72 object-cover object-center rounded-md",
+                className
+            )}
+        />
+    );
+}
--- src/views/admin/genres/_comp/index.ts
+++ src/views/admin/genres/_comp/index.ts
@@ -1,5 +1,8 @@
 import {GenreIndexCard} from "@/views/admin/genres/_comp/GenreIndexCard.tsx";
+import {GenreImageBanner} from "@/views/admin/genres/_comp/images/GenreImageBanner.tsx";
 
 export {
-    GenreIndexCard
+    GenreIndexCard,
+    GenreImageBanner,
 }
+
