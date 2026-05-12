142de5ea2486a3b5850596f53bce9c2f1d4d3178
hadumbae <cmcdnn6919@gmail.com>
2026-05-12 20:42:16 +0700

    feat: restyle movie index card

--- src/views/admin/movies/_comp/index-list-display/MovieIndexCard.tsx
+++ src/views/admin/movies/_comp/index-list-display/MovieIndexCard.tsx
@@ -1,15 +1,14 @@
 /** @fileoverview Compact movie index card component for administrative listings. */
 
-import {Card, CardContent} from "@/common/components/ui/card.tsx";
+import {Card, CardContent, CardHeader} from "@/common/components/ui/card.tsx";
 import {Info} from "lucide-react";
 import TooltipButton from "@/common/components/buttons/TooltipButton.tsx";
 import {MovieIndexDetailsDialog} from "@/views/admin/movies/_comp/index-list-display/MovieIndexDetailsDialog.tsx";
-import LoggedHoverLink from "@/common/components/navigation/logged-link/LoggedHoverLink.tsx";
 import {cn} from "@/common/lib/utils.ts";
 import {MovieDetails} from "@/domains/movies/schema/movie/MovieDetailsSchema.ts";
 import {MoviePosterImageDialog} from "@/views/admin/movies/_comp/poster-image";
 import {ReactElement} from "react";
-import {formatMovieData} from "@/domains/movies/_feat/formatters";
+import {MovieMetaGenreBadges, MovieMetaRow} from "@/views/admin/movies/_comp/movie-details";
 
 /** Props for the MovieIndexCard component. */
 type IndexCardProps = {
@@ -21,29 +20,20 @@ type IndexCardProps = {
  * Renders a concise movie overview including a poster, title link, and metadata.
  */
 export function MovieIndexCard({movie, className}: IndexCardProps): ReactElement {
-    const {
-        slug,
-        title,
-        formatted: {posterURL, genreList, yearAndDuration},
-    } = formatMovieData(movie);
+    const {genres, posterImage} = movie;
 
     return (
         <Card>
-            <CardContent className="p-4 flex items-center space-x-2">
-                <section>
-                    <MoviePosterImageDialog
-                        url={posterURL}
-                        className={className}
-                    />
-                </section>
-
-                <section className="flex-grow flex flex-col space-y-1">
-                    <LoggedHoverLink to={`/admin/movies/get/${slug}`}>
-                        <h1 className="text-lg font-bold">{title}</h1>
-                    </LoggedHoverLink>
-
-                    <h2 className="text-sm text-neutral-400">{yearAndDuration}</h2>
-                    <h3 className="text-xs text-neutral-400">{genreList}</h3>
+            <CardHeader className="p-0">
+                <MoviePosterImageDialog
+                    url={posterImage?.secure_url}
+                    className="w-full h-72 rounded-b-none"
+                />
+            </CardHeader>
+            <CardContent className={cn("p-5 flex items-center justify-between", className)}>
+                <section className="space-y-2">
+                    <MovieMetaRow movie={movie}/>
+                    <MovieMetaGenreBadges genres={genres} />
                 </section>
 
                 <MovieIndexDetailsDialog movie={movie}>
--- src/views/admin/movies/index-page/content.tsx
+++ src/views/admin/movies/index-page/content.tsx
@@ -4,7 +4,6 @@
  * based on administrative movie data.
  */
 
-import SectionHeader from "@/common/components/page/SectionHeader.tsx";
 import {PageFlexWrapper} from "@/views/common/_comp/page";
 import MovieIndexPageHeader from "@/views/admin/movies/index-page/header.tsx";
 import PresetFilterDialog from "@/common/components/dialog/PresetFilterDialog.tsx";
@@ -17,6 +16,7 @@ import {MovieDetails} from "@/domains/movies/schema/movie/MovieDetailsSchema.ts"
 import {ReactElement} from "react";
 import EmptyArrayContainer from "@/common/components/text/EmptyArrayContainer.tsx";
 import {MovieIndexCard} from "@/views/admin/movies/_comp/index-list-display";
+import {SROnly} from "@/views/common/_comp/screen-readers";
 
 type ContentProps = {
     page: number;
@@ -37,21 +37,24 @@ export function MovieIndexPageContent(
 
     return (
         <PageFlexWrapper>
-            <MovieIndexPageHeader />
+            <MovieIndexPageHeader/>
 
             <PresetFilterDialog
                 title="Movie Filters"
                 description="Filter and sort movies here."
             >
-                <MovieQueryOptionFormContainer presetValues={searchParams} />
+                <MovieQueryOptionFormContainer presetValues={searchParams}/>
             </PresetFilterDialog>
 
             {movies.length > 0 ? (
-                <section className="grid grid-cols-1 lg:grid-cols-2 gap-3">
-                    <SectionHeader srOnly={true}>List Of Movies</SectionHeader>
+                <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
+                    <SROnly text="List Of Movies"/>
 
                     {movies.map((movie: MovieDetails) => (
-                        <MovieIndexCard className="w-16" movie={movie} key={movie._id} />
+                        <MovieIndexCard
+                            key={movie._id}
+                            movie={movie}
+                        />
                     ))}
                 </section>
             ) : (
