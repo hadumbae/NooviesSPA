982903e5aa93f2db23798b5672b04f550ad0df91
hadumbae <cmcdnn6919@gmail.com>
2026-05-15 00:15:08 +0700

    fix: clean up barrel export index

--- src/views/admin/movies/details-page/index.ts
+++ src/views/admin/movies/details-page/index.ts
@@ -2,7 +2,6 @@ import {MovieDetailsPageContent} from "@/views/admin/movies/details-page/content
 import {MovieDetailsPage} from "@/views/admin/movies/details-page/page.tsx";
 import {MovieDetailsHeader} from "@/views/admin/movies/details-page/header.tsx";
 import {MovieDetailsBreadcrumb} from "@/views/admin/movies/details-page/breadcrumbs.tsx";
-import {MovieDetailsPageTabs} from "@/views/admin/movies/details-page/tabs/tabs.tsx";
 import {MovieDetailsPageCreditSection} from "./sections/creditSection.tsx";
 import {MovieDetailsPageShowingSection} from "./sections/showingSection.tsx";
 
@@ -11,7 +10,6 @@ export {
     MovieDetailsPageContent,
     MovieDetailsHeader,
     MovieDetailsBreadcrumb,
-    MovieDetailsPageTabs,
     MovieDetailsPageCreditSection,
     MovieDetailsPageShowingSection,
 }
