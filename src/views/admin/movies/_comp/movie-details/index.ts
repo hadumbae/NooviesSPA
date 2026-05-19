0cdb8645138d9b47b692420b28c6171a71baf4d6
hadumbae <cmcdnn6919@gmail.com>
2026-05-19 20:46:44 +0700

    fix: fix incorrect imports caused by Webstorm

--- src/views/admin/movies/_comp/movie-details/index.ts
+++ src/views/admin/movies/_comp/movie-details/index.ts
@@ -1,5 +1,5 @@
 import {MovieMetaGenreBadges} from "@/views/admin/movies/_comp/movie-details/MovieMetaGenreBadges.tsx";
-import {AdminMovieMeta} from "src/views/admin/movies/_comp/movie-details/AdminMovieMeta.tsx";
+import {AdminMovieMeta} from "@/views/admin/movies/_comp/movie-details/AdminMovieMeta.tsx";
 
 export {
     MovieMetaGenreBadges,
