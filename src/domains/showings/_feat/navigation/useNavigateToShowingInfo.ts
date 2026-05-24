42d989c94a28255c7000aa379e06f1f98a3bb470
hadumbae <cmcdnn6919@gmail.com>
2026-05-24 13:11:14 +0700

    feat: create showing navigation hooks

---
+++ src/domains/showings/_feat/navigation/useNavigateToShowingInfo.ts
@@ -0,0 +1,26 @@
+/**
+ * @fileoverview Hook for navigating to the showing's client-side view with logging.
+ */
+
+import useLoggedNavigate, {LoggingMessageParams} from "@/common/hooks/logging/useLoggedNavigate.ts";
+import {NavigateOptions} from "react-router-dom";
+import {SlugString} from "@/common/schema/strings/simple-strings/SlugString.ts";
+
+/** Configuration for the showing navigation action. */
+type NavConfig = LoggingMessageParams & {
+    slug: SlugString;
+    options: NavigateOptions,
+}
+
+/** Returns a function to navigate to a specific showing's client-side view. */
+export function useNavigateToShowingInfo() {
+    const navigate = useLoggedNavigate();
+
+    return ({slug, options, ...config}: NavConfig) => {
+        navigate({
+            to: `/browse/showings/${slug}`,
+            options,
+            ...config,
+        });
+    };
+}
