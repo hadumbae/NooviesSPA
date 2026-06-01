f9927006b5389ed3a20d80f453d15c468f9d5417
hadumbae <cmcdnn6919@gmail.com>
2026-06-01 15:54:05 +0700

    fix: remove `query` from movie review crud barrel export

--- src/domains/movieReviews/_feat/crud/repository/index.ts
+++ src/domains/movieReviews/_feat/crud/repository/index.ts
@@ -1,11 +1,10 @@
 import {MovieReviewCRUDBaseURL} from "@/domains/movieReviews/_feat/crud/repository/baseURL.ts";
-import {destroy, findByID, findBySlug, paginated, query} from "@/domains/movieReviews/_feat/crud/repository/repository.ts";
+import {destroy, findByID, findBySlug, paginated} from "@/domains/movieReviews/_feat/crud/repository/repository.ts";
 
 export {
     MovieReviewCRUDBaseURL,
     findByID,
     findBySlug,
-    query,
     paginated,
     destroy,
 }
